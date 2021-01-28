79be12db1905dc31fb3effc8daf14f25ef636441
YihengYe <yiy291@ucsd.edu>
2021-01-28 12:10:55 -0800

    top1000_article

--- etl.py
+++ etl.py
@@ -3,6 +3,19 @@ import sys
 import json
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+
+def top_1000():
+    '''
+    get top 1000 articles of COVID in wikipedia from 2020/1/1 to 2020/12/31
+    '''
+    url="https://en.wikipedia.org/wiki/Wikipedia:WikiProject_COVID-19/Popular_pages"
+    response=requests.get(url)
+    page=BeautifulSoup(response.text, 'html.parser')
+    table=page.find_all('table')[3]
+    readable=pd.read_html(table.prettify())
+    readable[0].to_csv("data/raw/top1000.csv", index=False)
+    return
 
 def api_getpageview(article):
     '''
@@ -13,7 +26,9 @@ def api_getpageview(article):
 
     return: A csv contained information
     '''
-    url="https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{1}/daily/2020010100/2020123100".format(article)
+    url="https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{0}/daily/2020010100/2020123100".format(article)
     response=requests.get(url)
+    if '200' in response:
+        print('ok')
 
     return
--- run.py
+++ run.py
@@ -2,14 +2,20 @@
 
 import sys
 import json
+import pandas as pd
 from etl import *
 
 
 def main(targets):
     if 'data' in targets:
         
-      pass
-
+      with open('config/data-params.json') as fh:
+        data_cfg=json.load(fh)
+        
+      top_1000()
+      tops=pd.read_csv(data_cfg['articles'])['Page title'].values
+      for i in tops:
+        print(i)
     return
 
 
