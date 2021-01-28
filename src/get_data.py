80ea6825df6648d63cc7aa97ece46334c33f4ad3
YihengYe <yiy291@ucsd.edu>
2021-01-28 11:20:54 -0800

    working_on

--- src/get_data.py
+++ src/get_data.py
@@ -1,8 +1,18 @@
 import os
-import pymysql
 import sys
 import json
 import requests
-from datetime import date
-from datetime import datetime
-import time as tts
+import pandas as pd
+
+def api_get(article, category):
+    '''
+    Using GET method to get page data since 2020010100 (2020/1/1) to 2020123100 (2020/12/31)
+
+    article: The article we are looking for
+    category: Type of data get
+
+    return: A csv contained information
+    '''
+    get="https://wikimedia.org/api/rest_v1/metrics/{1}/per-article/en.wikipedia/all-access/all-agents/{2}/daily/2020010100/2020123100".format(category,article)
+
+    return
