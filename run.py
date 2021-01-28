5c7867c4afe700debfabc5d30f9bf3bdc838b8e8
YihengYe <yiy291@ucsd.edu>
2021-01-28 11:07:21 -0800

    repo setup

---
+++ run.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import sys
+import json
+
+
+def main(targets):
+    if 'data' in targets:
+        
+      pass
+
+    return
+
+
+
+
+
+
+if __name__ == "__main__":
+    targets=sys.argv[1: ]
+    main(targets)
---
+++ src/get_data.py
@@ -0,0 +1,8 @@
+import os
+import pymysql
+import sys
+import json
+import requests
+from datetime import date
+from datetime import datetime
+import time as tts
